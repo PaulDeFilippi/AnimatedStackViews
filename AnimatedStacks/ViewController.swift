bb4697a5c67d9bb41fa8cffe3010a08e91a14c6c
Paul Defilippi <BlindFaith@pauls-imac.home>
2016-11-23 13:33:58 -0500

    initial commit

--- AnimatedStacks/ViewController.swift
+++ AnimatedStacks/ViewController.swift
@@ -10,16 +10,70 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var emojiView: [UIView]! {
+        didSet {
+            emojiView.forEach {
+                $0.isHidden = true
+            }
+        }
+    }
+    
+    @IBOutlet weak var emojiText: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+   
+    @IBAction func ipadButtonPressed(_ sender: Any) {
+        hideButtons()
+        emojiText.text = "📲"
     }
-
+    
+    @IBAction func partyTimeButtonPressed(_ sender: Any) {
+        hideButtons()
+        emojiText.text = "🎉"
+    }
+    
+    @IBAction func hotButtonPressed(_ sender: Any) {
+        hideButtons()
+        emojiText.text = "🔥"
+    }
+    
+    @IBAction func worldButtonPressed(_ sender: Any) {
+        hideButtons()
+        emojiText.text = "🌎"
+    }
+    
+    @IBAction func familyTimeButtonPressed(_ sender: Any) {
+        hideButtons()
+        emojiText.text = "👨‍👩‍👧‍👦"
+    }
+    
+    @IBAction func christmasButtonPressed(_ sender: Any) {
+        hideButtons()
+        emojiText.text = "🎅🏼"
+    }
+    
+    @IBAction func pizzaButtonPressed(_ sender: Any) {
+        hideButtons()
+        emojiText.text = "🍕"
+    }
+    
+    @IBAction func onSettingsButtonPressed(_ sender: Any) {
+        hideButtons()
+        emojiText.text = "⚙"
+    }
+    
+    func hideButtons() {
+        UIView.animate(withDuration: 0.3) {
+            self.emojiView.forEach {
+                $0.isHidden = !$0.isHidden
+            }
+        }
+    }
 
 }
 
